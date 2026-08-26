264726e17a8e44ad7d6e6617ef892c436ad72d38
junior cueva <junior.cueva@tecsup.edu.pe>
2026-08-26 10:22:30 -0500

    paso1:lectura de datos y calculo del monto total

---
+++ semana 02/tienda1/tienda1.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+print("Nombre del producto:")
+let producto = readLine() ?? ""
+
+print("Precio unitario:")
+let precioUnit = Double(readLine() ?? "0") ?? 0.0
+
+print("Cantidad:")
+let cantidad = Int(readLine() ?? "0") ?? 0
+
+let montoTotalCompra = precioUnit * Double(cantidad)
+
+print("\n-----------------------------------")
+print("MONTO TOTAL DE LA COMPRA: S/ \(String(format: "%.2f", montoTotalCompra))")
+print("-------------------------------------")
