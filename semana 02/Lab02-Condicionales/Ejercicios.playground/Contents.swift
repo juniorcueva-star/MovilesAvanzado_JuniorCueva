12abbb3c1f1fc11f9cc0bec7eb6fe4ece457e12a
Junior Cueva <junior.cueva@tecsup.edu.pe>
2026-09-01 20:58:26 -0500

    feat: Crear proyecto Lab02 y Playground en Semana02

---
+++ semana 02/Lab02-Condicionales/Ejercicios.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+// Docente : Juan León S.
+// Alumno  : Junior Cueva
+// Curso   : Programación en Móviles Avanzado
+// Lab 02  : Estructuras Condicionales y Bucles
+
+var saludo = "Hola, playground"
+print(saludo)
