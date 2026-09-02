1f34bba32da59aafb32a5683379a2294a39de6ef
Junior Cueva <junior.cueva@tecsup.edu.pe>
2026-09-01 22:53:58 -0500

    feat: Completar ejercicio 2 - switch

--- semana 02/Lab02-Condicionales/Ejercicios.playground/Contents.swift
+++ semana 02/Lab02-Condicionales/Ejercicios.playground/Contents.swift
@@ -121,3 +121,83 @@ if esLunes && llueve {
 } else {
     print("Ni lunes ni llueve")
 } 
+
+
+// ===== EJERCICIO 2: SWITCH =====
+
+// --- Ejemplo (ya resuelto): ---
+let diaSemana = 3
+switch diaSemana {
+case 1: print("Lunes")
+case 2: print("Martes")
+case 3: print("Miércoles")
+case 4: print("Jueves")
+case 5: print("Viernes")
+case 6: print("Sábado")
+case 7: print("Domingo")
+default: print("Día inválido")
+}
+
+// --- TODO 4: Clasificar nota numérica a letra ---
+let notaSwitch = 16
+switch notaSwitch {
+case 18...20: print("A - Excelente")
+case 15...17: print("B - Bueno")
+case 13...14: print("C - Aprobado")
+case 11...12: print("D - Desaprobado")
+case 0...10: print("F - Reprobado")
+default: print("Nota inválida")
+}
+
+
+// --- TODO 5: Calculadora simple con switch ---
+let num1 = 20.0
+let num2 = 5.0
+let operacion = "+"
+switch operacion {
+case "+": print("Resultado: \(num1 + num2)")
+case "-": print("Resultado: \(num1 - num2)")
+case "*": print("Resultado: \(num1 * num2)")
+case "/":
+    if num2 != 0 {
+        print("Resultado: \(num1 / num2)")
+    } else {
+        print("Error: no se puede dividir entre cero")
+    }
+default: print("Operación no válida")
+}
+
+
+// --- TODO 6: Categoría de producto por precio ---
+// let precio = 350.0
+
+// Categorías: Económico (0-99), Medio (100-499),
+//	Premium (500-999), Lujo (1000+)
+// Usa switch con rangos para clasificar
+
+let precio = 350.0
+switch precio {
+case 0..<100: print("Económico")
+case 100..<500: print("Medio")
+case 500..<1000: print("Premium")
+case 1000...: print("Lujo")
+default: print("Precio inválido")
+}
+
+
+// 2.2 predicciones
+//===== PREDICT: ¿Qué imprime? =====
+
+let mes = 2
+switch mes {
+case 1, 3, 5, 7, 8, 10, 12: print("31 días")
+case 4, 6, 9, 11: print("30 días")
+case 2: print("28 o 29 días")
+default: print("Mes inválido")
+} 
+
+let letra: Character = "a"
+switch letra {
+case "a", "e", "i", "o", "u": print("Vocal")
+default: print("Consonante")
+} 
