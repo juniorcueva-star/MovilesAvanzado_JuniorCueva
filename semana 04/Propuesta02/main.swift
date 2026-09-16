2495c97fb88cfab94412b0c12b77087fad9fc9dc
Junior Cueva <junior.cueva@tecsup.edu.pe>
2026-09-15 23:31:15 -0500

    feat: Propuesta 02 - subclase ClienteNatural con override

--- semana 04/Propuesta02/main.swift
+++ semana 04/Propuesta02/main.swift
@@ -25,3 +25,27 @@ class Cliente {
         print("Monto minimo de apertura: S/ \(String(format: "%.2f", montoMinimoApertura))")
     }
 }
+
+// CLIENTE NATURAL 
+class ClienteNatural: Cliente {
+    let nombreCompleto: String
+    let dni: String
+
+    init(codigo: String, direccion: String, fechaDeRegistro: String,
+         numeroCuenta: String, montoMinimoApertura: Double,
+         nombreCompleto: String, dni: String) {
+        self.nombreCompleto = nombreCompleto
+        self.dni = dni
+        super.init(codigo: codigo, direccion: direccion,
+                   fechaDeRegistro: fechaDeRegistro,
+                   numeroCuenta: numeroCuenta,
+                   montoMinimoApertura: montoMinimoApertura)
+    }
+
+    override func mostrarDatos() {
+        print("Cliente Natural:")
+        print("Nombre: \(nombreCompleto)")
+        print("DNI: \(dni)")
+        super.mostrarDatos()
+    }
+}
